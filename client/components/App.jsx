ac1e278dd37c5f0140ff9fc53fe666c0b250e697
bulknskull <bhepfing@gmail.com>
2021-04-20 16:53:22 -0400

    successfully fetching data via React/Express

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,57 @@
 const React = require('react');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchedWords: false,
+      words: []
+    }
+  }
+
+  componentDidMount() {
+    console.log('mounting...');
+    fetch('/dictionary')
+      .then(res => res.json())
+      .then((words) => {
+        return this.setState({
+          words,
+          fetchedWords: true
+        });
+      })
+      .catch(err => console.log(err + 'Error upon mount'))
+  }
+
+  getDictionary() {
+    // make GET request for entirety of dictionary file, processed accordingly
+    console.log('egg');
+    fetch('/dictionary')
+      .then(res => res.json())
+      .then((words) => {
+        console.log(words[0]['subcat']);
+      })
+      .catch(err => console.log('There was an error'));
+  }
+
   render() {
     return (
-      <div>
-        <p>Langdoc is a language-learning app that lets you design your own curriculum.</p>
-        <p>Choose an option to get started:</p>
-        <div>
-          <button>View dictionary</button>
-          <button>Add a word</button>
-          <button>Remove a word</button>
-          <button>Update a word</button>
-          <button>Get a flash card</button>
+      <div id='body'>
+        <div id='hello'>
+          <h1>Welcome to Langdoc!</h1>
+          <p>Langdoc is a language-learning app that lets you design your own curriculum.</p>
+          <p>Choose an option below:</p>
+        </div>
+        <div id='buttons'>
+          <button onClick={this.getDictionary}>View dictionary</button>
+        </div>
+        <div id='display'>
+
         </div>
       </div>
     )
   }
 }
+
+// buttons will be within CONTAINER that inherits/is rendered by App
 
 export default App;
